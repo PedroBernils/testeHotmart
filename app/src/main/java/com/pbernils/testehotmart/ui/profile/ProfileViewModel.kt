4fc2513e2e07b1940c5ed3f65d62ed4ad5699c6a
Pedro Bernils <pedro@incubation.studio>
2021-06-21 12:17:00 -0300

    Cleanup from last commit

--- app/src/main/java/com/pbernils/testehotmart/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/pbernils/testehotmart/ui/profile/ProfileViewModel.kt
@@ -1,10 +1,10 @@
-package com.pbernils.testehotmart.ui.notifications
+package com.pbernils.testehotmart.ui.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "Perfil"
